6f3c707f46757d3267b19fec88de82ed0c604e13
samukhin <47834890+samukhin@users.noreply.github.com>
2023-07-10 15:05:41 +0300

    Create entrypoint.sh

---
+++ v1/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+tini -- rancher --http-listen-port=80 --https-listen-port=443 --audit-log-path=${AUDIT_LOG_PATH} --audit-level=${AUDIT_LEVEL} --audit-log-maxage=${AUDIT_LOG_MAXAGE} --audit-log-maxbackup=${AUDIT_LOG_MAXBACKUP} --audit-log-maxsize=${AUDIT_LOG_MAXSIZE} --add-local=true&
+sleep 5
+kwok \
+  --kubeconfig=/etc/rancher/k3s/k3s.yaml \
+  --manage-all-nodes=true
+  --disregard-status-with-annotation-selector=kwok.x-k8s.io/status=custom \
+  --disregard-statusq-with-label-selector= \
+  --cidr=10.0.0.1/24 \
+  --node-ip=10.0.0.1
