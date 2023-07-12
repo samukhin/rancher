f1313519ca8f227f49caa550c0f48e37b55d6e83
samukhin <47834890+samukhin@users.noreply.github.com>
2023-07-12 15:22:07 +0300

    Update entrypoint.sh

--- v3/entrypoint.sh
+++ v3/entrypoint.sh
@@ -13,8 +13,8 @@ tini -- rancher --http-listen-port=80 --https-listen-port=443 --audit-log-path=$
 
 kwok \
   --kubeconfig=/etc/rancher/k3s/k3s.yaml \
-  --manage-all-nodes=true
+  --manage-all-nodes=true \
   --disregard-status-with-annotation-selector=kwok.x-k8s.io/status=custom \
-  --disregard-statusq-with-label-selector= \
+  --disregard-status-with-label-selector= \
   --cidr=10.0.0.1/24 \
   --node-ip=10.0.0.1
