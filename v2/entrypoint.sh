78f48a29c1be13c5aad3988c105b9d0b96a7ee51
samukhin <47834890+samukhin@users.noreply.github.com>
2023-07-16 14:29:15 +0300

    Update entrypoint.sh

--- v2/entrypoint.sh
+++ v2/entrypoint.sh
@@ -5,4 +5,4 @@ set -e
 #KWOK_KUBE_VERSION=v1.21.14 kwokctl -c /tmp/exec.yaml create cluster
 kwokctl start cluster
 kwokctl get kubeconfig > /tmp/config
-tini -- rancher --http-listen-port=80 --https-listen-port=443 --audit-log-path=${AUDIT_LOG_PATH} --audit-level=${AUDIT_LEVEL} --audit-log-maxage=${AUDIT_LOG_MAXAGE} --audit-log-maxbackup=${AUDIT_LOG_MAXBACKUP} --audit-log-maxsize=${AUDIT_LOG_MAXSIZE} --kubeconfig=/tmp/config --add-local=true
+tini -- rancher --http-listen-port=80 --https-listen-port=443 --audit-log-path=${AUDIT_LOG_PATH} --audit-level=${AUDIT_LEVEL} --audit-log-maxage=${AUDIT_LOG_MAXAGE} --audit-log-maxbackup=${AUDIT_LOG_MAXBACKUP} --audit-log-maxsize=${AUDIT_LOG_MAXSIZE} --kubeconfig=/tmp/config --add-local=true --features fleet=false
