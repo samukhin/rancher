3c5332d500fc7ad35ac1cdb8371236bed3d78ba5
samukhin <47834890+samukhin@users.noreply.github.com>
2023-07-16 13:58:18 +0300

    Update entrypoint.sh

--- v2/entrypoint.sh
+++ v2/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-kwokctl delete cluster
-KWOK_KUBE_VERSION=v1.21.14 kwokctl -c /tmp/exec.yaml create cluster
+#kwokctl delete cluster
+#KWOK_KUBE_VERSION=v1.21.14 kwokctl -c /tmp/exec.yaml create cluster
+kwokctl start cluster
 kwokctl get kubeconfig > /tmp/config
 tini -- rancher --http-listen-port=80 --https-listen-port=443 --audit-log-path=${AUDIT_LOG_PATH} --audit-level=${AUDIT_LEVEL} --audit-log-maxage=${AUDIT_LOG_MAXAGE} --audit-log-maxbackup=${AUDIT_LOG_MAXBACKUP} --audit-log-maxsize=${AUDIT_LOG_MAXSIZE} --kubeconfig=/tmp/config --add-local=true
